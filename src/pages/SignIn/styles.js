2dbf98206c7d1116ad7f786879152e43f063e0dd
Fellipe Thomas <fellipecab@gmail.com>
2020-02-16 23:07:34 -0300

    Change Login screen background

--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -7,7 +7,7 @@ export const SContainer = Styled.section`
   flex-flow: column wrap;
   justify-content: center;
   align-items: center;
-  background-image: linear-gradient(45deg, lightblue, purple);
+  background-image: linear-gradient(339deg, rgba(141,10,255,1) 16%, rgba(0,151,255,1) 47%, rgba(141,10,255,1) 90%);
 `;
 
 export const SPanel = Styled.div`
