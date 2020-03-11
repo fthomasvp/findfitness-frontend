8142a0dafe0bfb2e3a32ed3496f998a1932c1b0d
Fellipe Thomas <fellipecab@gmail.com>
2020-03-11 17:32:15 -0300

    Add validators for Person fields

--- src/pages/validators/index.js
+++ src/pages/validators/index.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import moment from 'moment';
 
 export const email = Yup.string()
   .email('Digite um e-mail válido')
@@ -7,6 +8,25 @@ export const email = Yup.string()
 export const password = Yup.string()
   .min(6, 'A senha deve conter pelo menos 6 caracteres')
   .required('Preencha o campo de senha');
+
+export const name = Yup.string()
+  .min(2, 'Deve conter pelo menos 2 caracteres')
+  .max(140, 'Deve conter até 140 caracteres')
+  .required('Preencha o campo');
+
+export const phone = Yup.string()
+  .matches(/\d/, 'Deve conter apenas números')
+  .min(11, 'Deve conter pelo menos 11 caracteres')
+  .required('Preencha o campo');
+
+export const cpf = Yup.string()
+  .matches(/\d/, 'Deve conter apenas números')
+  .min(11, 'Deve conter pelo menos 11 caracteres')
+  .required('Preencha o campo');
+
+export const birthdate = Yup.date()
+  .max(moment().subtract(18, 'years'), 'Precisa ter pelo menos 18 anos ')
+  .required('Preencha o campo');
 
 const YupSchema = fields => {
   return Yup.object().shape(fields);
