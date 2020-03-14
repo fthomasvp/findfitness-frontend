25d11b0c97f6ab5edddf6158cf9ed8a8c4690754
Fellipe Thomas <fellipecab@gmail.com>
2020-03-14 19:19:04 -0300

    Update Types

--- src/store/ducks/SignIn.js
+++ src/store/ducks/SignIn.js
@@ -1,9 +1,9 @@
 /**
  * Action Types
  * */
-export const SIGN_IN_REQUEST = 'auth/SIGN_IN_REQUEST';
-export const SIGN_IN_SUCCESS = 'auth/SIGN_IN_SUCCESS';
-export const SIGN_IN_FAIL = 'auth/SIGN_IN_FAIL';
+export const SIGN_IN_REQUEST = '@auth/SIGN_IN_REQUEST';
+export const SIGN_IN_SUCCESS = '@auth/SIGN_IN_SUCCESS';
+export const SIGN_IN_FAIL = '@auth/SIGN_IN_FAIL';
 
 /**
  * Action Creators
