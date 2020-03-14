2dda2237047d449c78db35e17c635b2924a1dfad
Fellipe Thomas <fellipecab@gmail.com>
2020-03-14 03:05:06 -0300

    Add Address validations and error messages

--- src/pages/validators/index.js
+++ src/pages/validators/index.js
@@ -1,6 +1,12 @@
 import * as Yup from 'yup';
 import moment from 'moment';
 
+export const errorMessages = {
+  requiredNumericField: 'Preencha o campo apenas com números',
+  specialCharactersField:
+    'Não pode conter letras, espaços em branco ou caracteres especiais',
+};
+
 export const email = Yup.string()
   .email('Digite um e-mail válido')
   .required('Preencha o campo de e-mail');
@@ -17,6 +23,51 @@ export const name = Yup.string()
 export const birthdate = Yup.date()
   .max(moment().subtract(18, 'years'), 'Precisa ter pelo menos 18 anos ')
   .required('Preencha o campo');
+
+export const street = Yup.string()
+  .min(4, 'Deve conter pelo menos 4 caracteres')
+  .max(200, 'Não pode ultrapassar 200 caracteres')
+  .required('Preencha o campo apenas com números');
+
+export const state = Yup.string()
+  .min(2, 'Deve conter pelo menos 2 letras')
+  .test(
+    'state',
+    'Não pode conter números, espaços em branco ou caracteres especiais',
+    value => /^[a-zA-Z]+$/gm.test(value)
+  )
+  .required('Preencha o campo apenas com duas letras');
+
+export const zipcode = Yup.number()
+  .typeError(
+    'Não pode conter letras, espaços em branco ou caracteres especiais'
+  )
+  .test(
+    'zipcode',
+    'Deve conter 8 caracteres numéricos',
+    value => String(value).length === 8
+  )
+  .required('Preencha o campo apenas com números');
+
+export const number = Yup.number().typeError(
+  'Não pode conter letras, espaços em branco ou caracteres especiais'
+);
+
+export const city = Yup.string()
+  .trim('Não pode conter apenas espaços em branco')
+  .test('city', 'Não pode conter números ou caracteres especiais', value =>
+    /^[a-zA-Z ]+$/gm.test(value)
+  )
+  .required('Preencha o campo apenas com letras');
+
+export const neighborhood = Yup.string()
+  .trim('Não pode conter apenas espaços em branco')
+  .test(
+    'neighborhood',
+    'Não pode conter números ou caracteres especiais',
+    value => /^[a-zA-Z ]+$/gm.test(value)
+  )
+  .required('Preencha o campo apenas com letras');
 
 const YupSchema = fields => {
   return Yup.object().shape(fields);
