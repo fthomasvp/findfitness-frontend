14c43953c3bd1a61d8019e460f706830337e3e39
Fellipe Thomas <fellipecab@gmail.com>
2020-03-14 19:18:51 -0300

    Update SignUp Types, Actions and Reducer logic

--- src/store/ducks/SignUp.js
+++ src/store/ducks/SignUp.js
@@ -1,58 +1,57 @@
 /**
  * Action Types
  * */
-// export const SIGN_UP_REQUEST = '@signup/SIGN_UP_REQUEST';
-// export const SIGN_UP_SUCCESS = '@signup/SIGN_UP_SUCCESS';
-// export const SIGN_UP_FAIL = '@signup/SIGN_UP_FAIL';
-export const UPDATE_PROFILE_TYPE = '@signup/UPDATE_PROFILE_TYPE';
-export const UPDATE_PERSON_FORM = '@signup/UPDATE_PERSON_FORM';
-export const UPDATE_ADDRESS_FORM = '@signup/UPDATE_ADDRESS_FORM';
+export const STORE_PROFILE_TYPE = '@signup/STORE_PROFILE_TYPE';
+export const STORE_PERSON_FORM = '@signup/STORE_PERSON_FORM';
+export const STORE_ADDRESS_FORM = '@signup/STORE_ADDRESS_FORM';
+
+export const SIGN_UP_REQUEST = '@signup/SIGN_UP_REQUEST';
+export const SIGN_UP_SUCCESS = '@signup/SIGN_UP_SUCCESS';
+export const SIGN_UP_FAIL = '@signup/SIGN_UP_FAIL';
 
 /**
  * Action Creators
  * */
-export const updateProfileType = profileType => {
+export const storeProfileType = profileType => {
   return {
-    type: UPDATE_PROFILE_TYPE,
+    type: STORE_PROFILE_TYPE,
     profileType,
   };
 };
 
-export const updatePersonForm = person => {
+export const storePersonForm = person => {
   return {
-    type: UPDATE_PERSON_FORM,
+    type: STORE_PERSON_FORM,
     person,
   };
 };
 
-export const updateAddressForm = address => {
+export const storeAddressForm = address => {
   return {
-    type: UPDATE_ADDRESS_FORM,
+    type: STORE_ADDRESS_FORM,
     address,
   };
 };
 
-// export const signUpRequest = userToCreate => {
-//   return {
-//     type: SIGN_UP_REQUEST,
-//     userToCreate,
-//   };
-// };
+export const signUpRequest = userToCreate => {
+  return {
+    type: SIGN_UP_REQUEST,
+    userToCreate,
+  };
+};
 
-// export const signUpSuccess = ({ data, headers }) => {
-//   return {
-//     type: SIGN_UP_SUCCESS,
-//     data,
-//     headers,
-//   };
-// };
+export const signUpSuccess = () => {
+  return {
+    type: SIGN_UP_SUCCESS,
+  };
+};
 
-// export const signUpFail = error => {
-//   return {
-//     type: SIGN_UP_FAIL,
-//     error,
-//   };
-// };
+export const signUpFail = error => {
+  return {
+    type: SIGN_UP_FAIL,
+    error,
+  };
+};
 
 /**
  * Reducer
@@ -96,7 +95,7 @@ const initialState = {
 
 export const signUp = (state = initialState, action) => {
   switch (action.type) {
-    case UPDATE_PROFILE_TYPE:
+    case STORE_PROFILE_TYPE: {
       return {
         ...state,
         userToCreate: {
@@ -104,8 +103,9 @@ export const signUp = (state = initialState, action) => {
           profileType: action.profileType,
         },
       };
+    }
 
-    case UPDATE_PERSON_FORM: {
+    case STORE_PERSON_FORM: {
       const { profileType } = state.userToCreate;
       const key = profileType === 'STUDENT' ? 'student' : 'personal';
 
@@ -118,7 +118,7 @@ export const signUp = (state = initialState, action) => {
       };
     }
 
-    case UPDATE_ADDRESS_FORM: {
+    case STORE_ADDRESS_FORM: {
       return {
         ...state,
         userToCreate: {
@@ -128,31 +128,59 @@ export const signUp = (state = initialState, action) => {
       };
     }
 
-    // case SIGN_UP_REQUEST:
-    //   return {
-    //     ...state,
-    //     loading: true,
-    //     user: action.payload,
-    //   };
+    case SIGN_UP_REQUEST: {
+      return {
+        ...state,
+        loading: true,
+      };
+    }
 
-    // case SIGN_UP_SUCCESS:
-    //   return {
-    //     ...state,
-    //     loading: false,
-    //     user: {
-    //       email: action.data.email,
-    //       profile: action.data.authorities[0].authority,
-    //     },
-    //     isAuthenticated: true,
-    //   };
+    case SIGN_UP_SUCCESS: {
+      return {
+        ...state,
+        loading: false,
+        userToCreate: {
+          profileType: 'STUDENT',
+          personal: {
+            name: '',
+            cref: '',
+            phone: '',
+            cpf: '',
+            password: '',
+            gender: 'M',
+            birthdate: '',
+            email: '',
+          },
+          student: {
+            name: '',
+            phone: '',
+            cpf: '',
+            password: '',
+            gender: 'M',
+            birthdate: '',
+            email: '',
+          },
+          address: {
+            street: '',
+            number: '',
+            complement: '',
+            neighborhood: '',
+            referenceLocation: '',
+            city: '',
+            state: '',
+            zipcode: '',
+          },
+        },
+      };
+    }
 
-    // case SIGN_UP_FAIL:
-    //   return {
-    //     ...state,
-    //     loading: false,
-    //     isAuthenticated: false,
-    //     error: { ...action.error, description: 'Email ou Senha inválido' },
-    //   };
+    case SIGN_UP_FAIL: {
+      return {
+        ...state,
+        loading: false,
+        error: { ...action.error },
+      };
+    }
 
     default:
       return state;
