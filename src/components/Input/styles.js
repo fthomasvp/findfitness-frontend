e8e5b8e9583cd832e260922eece652f343be1f8d
Fellipe Thomas <fellipecab@gmail.com>
2020-04-12 00:48:12 -0300

    Change default height of Input component

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,7 +1,7 @@
 import Styled from 'styled-components';
 
 export const Input = Styled.input`
-  height: ${({ height }) => height || '25px'};
+  height: ${({ height }) => height || '40px'};
   width: ${({ width }) => width || 'auto'};
   padding: ${({ padding }) => padding || '5px'};
   border-radius: ${({ borderRadius }) => borderRadius || '5px'};
