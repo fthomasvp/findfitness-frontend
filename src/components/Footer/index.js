ee3cf000e6fc9be79dec0cbb2568d230e3e6b533
Fellipe Thomas <fellipecab@gmail.com>
2020-04-16 05:31:12 -0300

    Add Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import oxentechLogo from '../../assets/images/oxentech-bird-logo.png';
+
+const Footer = () => {
+  return (
+    <>
+      <img src={oxentechLogo} alt="Logo da Empresa Oxentech" />
+    </>
+  );
+};
+
+export default Footer;
