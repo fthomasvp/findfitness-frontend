fda22891cc8a9390150d7145d597d9eded48a51e
Fellipe Thomas <fellipecab@gmail.com>
2020-04-25 22:35:54 -0300

    Remove Axios token variable assignment

--- src/store/sagas/Auth.js
+++ src/store/sagas/Auth.js
@@ -32,9 +32,6 @@ export function* signIn(action) {
     const response = yield call(API.post, '/login', action.payload);
 
     if (response && response.status === 200) {
-      API.defaults.headers.common['Authorization'] =
-        response.headers.authorization;
-
       yield put(signInSuccess(response));
     }
   } catch (error) {
