786ee39a7b76bbfc0b510bb2e1d8e7bbd47dfcd3
Fellipe Thomas <fellipecab@gmail.com>
2020-04-26 14:37:28 -0300

    Improve Authentication of private routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Switch, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
 import SignIn from '../pages/SignIn';
 import ProfileForm from '../pages/SignUp/ProfileForm';
 import StudentOrPersonalForm from '../pages/SignUp/StudentOrPersonalForm';
@@ -10,14 +9,13 @@ import Chat from '../pages/Chat';
 import Specialization from '../pages/Specialization';
 import StudentGroup from '../pages/StudentGroup';
 import Exercise from '../pages/Exercise';
-
 import MainLayout from '../components/MainLayout';
 import Menu from '../components/Menu';
-
 import API from '../services/API';
 
 const Routes = () => {
-  const { token } = useSelector(state => state.auth.user);
+  const { user, isAuthenticated } = useSelector(state => state.auth);
+  const { token } = user;
 
   if (token && token.length > 0) {
     API.defaults.headers.common['Authorization'] = token;
@@ -46,18 +44,22 @@ const Routes = () => {
 
         {/* Home */}
         <Route exact path="/studentgroup">
+          {!isAuthenticated && <Redirect to="/" />}
           <MainLayout menuArea={Menu} asideArea={StudentGroup} />
         </Route>
 
         <Route exact path="/specialization">
+          {!isAuthenticated && <Redirect to="/" />}
           <MainLayout menuArea={Menu} asideArea={Specialization} />
         </Route>
 
         <Route exact path="/exercise">
+          {!isAuthenticated && <Redirect to="/" />}
           <MainLayout menuArea={Menu} asideArea={Exercise} />
         </Route>
 
         <Route exact path="/chat">
+          {!isAuthenticated && <Redirect to="/" />}
           <MainLayout menuArea={Menu} asideArea={Chat} />
         </Route>
       </Switch>
