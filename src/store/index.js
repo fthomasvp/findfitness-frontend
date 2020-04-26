b19570fdbfe6ac04beb81b9d89d0436df6a0bc02
Fellipe Thomas <fellipecab@gmail.com>
2020-04-25 22:37:34 -0300

    Add Redux Persist and store data in Session

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,32 @@
-import { createStore, compose, applyMiddleware } from "redux";
-import createSagaMiddleware from "redux-saga";
+import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { persistStore, persistReducer } from 'redux-persist';
+import session from 'redux-persist/lib/storage/session';
+import reducers from './ducks';
+import sagas from './sagas';
+import Reactotron from '../config/ReactotronConfig';
 
-import reducers from "./ducks";
-import sagas from "./sagas";
+const persistConfig = {
+  key: 'findfitness',
+  storage: session,
+  whitelist: ['auth', 'studentGroup'],
+};
 
-import Reactotron from "../config/ReactotronConfig";
+const persistedReducer = persistReducer(persistConfig, reducers);
 
-const sagaMonitor = Reactotron.createSagaMonitor()
+const sagaMonitor = Reactotron.createSagaMonitor();
 
 const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 const middlewares = [sagaMiddleware];
 
 const composer =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV === 'development'
     ? compose(applyMiddleware(...middlewares), Reactotron.createEnhancer())
     : applyMiddleware(...middlewares);
 
-const store = createStore(reducers, composer);
+let store = createStore(persistedReducer, composer);
+let persistor = persistStore(store);
 
 sagaMiddleware.run(sagas);
 
-export default store;
+export { store, persistor };
