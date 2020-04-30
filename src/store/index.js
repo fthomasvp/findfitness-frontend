a27449fc69572e02187f063f2fe8c072ad88e0c4
Fellipe Thomas <fellipecab@gmail.com>
2020-04-30 11:21:22 -0300

    Update Index store for Production enviroment

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,9 @@
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { persistStore, persistReducer } from 'redux-persist';
 import session from 'redux-persist/lib/storage/session';
 import reducers from './ducks';
 import sagas from './sagas';
-import Reactotron from '../config/ReactotronConfig';
 
 const persistConfig = {
   key: 'findfitness',
@@ -14,15 +13,10 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, reducers);
 
-const sagaMonitor = Reactotron.createSagaMonitor();
-
-const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+const sagaMiddleware = createSagaMiddleware();
 const middlewares = [sagaMiddleware];
 
-const composer =
-  process.env.NODE_ENV === 'development'
-    ? compose(applyMiddleware(...middlewares), Reactotron.createEnhancer())
-    : applyMiddleware(...middlewares);
+const composer = applyMiddleware(...middlewares);
 
 let store = createStore(persistedReducer, composer);
 let persistor = persistStore(store);
