f0a17d86945e3e61b351137e2c223030194cc587
Fellipe Thomas <fellipecab@gmail.com>
2020-06-03 02:40:55 -0300

    Add Tab and List to display data

--- src/pages/StudentGroup/DialogStudentGroupDetails/index.js
+++ src/pages/StudentGroup/DialogStudentGroupDetails/index.js
@@ -17,23 +17,32 @@ import PersonIcon from '@material-ui/icons/Person';
 import PriorityHighIcon from '@material-ui/icons/PriorityHigh';
 import Badge from '@material-ui/core/Badge';
 import GroupIcon from '@material-ui/icons/Group';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ImageIcon from '@material-ui/icons/Image';
 
 import Utils from '../../../utils';
 import PersonalPicture from '../../../assets/images/guilian-fremaux-personal.jpg';
-import { DialogTitle, DialogContent, DialogActions } from './styles';
+import { DialogContent, DialogActions } from './styles';
 import DialogEnrollStudentGroup from '../../StudentGroup/DialogEnrollStudentGroup';
 
 const DialogStudentGroupDetails = ({ open, handleClose, studentGroup }) => {
   const {
     completeAddress,
+    contactPhone,
     exercises,
     eventDateTimeBegin,
     eventDateTimeEnd,
     eventPrice,
     maxStudentGroupAmount,
+    payments,
     personal,
     students,
-    contactPhone,
   } = studentGroup;
 
   const formatedAddress = Utils.formatAddress(completeAddress);
@@ -42,6 +51,10 @@ const DialogStudentGroupDetails = ({ open, handleClose, studentGroup }) => {
 
   // This will be valid only if the user is a Student
   const idStudent = useSelector(state => state.auth.user.id);
+
+  const [tab, setTab] = useState(0);
+
+  const handleChangeTab = (event, tabValue) => setTab(tabValue);
 
   /**
    * Variables and functions to DialogEnrollStudentGroup
@@ -67,198 +80,240 @@ const DialogStudentGroupDetails = ({ open, handleClose, studentGroup }) => {
       maxWidth={'md'}
       fullWidth
     >
-      <DialogTitle
-        id="student_group-details"
-        onClose={handleClose}
-        style={{ textAlign: 'center' }}
-      >
-        Detalhes da aula
-      </DialogTitle>
-      <DialogContent dividers>
-        {/* Personal info */}
-        <Typography gutterBottom variant="h5" align="center">
-          <PersonIcon /> PERSONAL
-        </Typography>
-        <Avatar
-          src={PersonalPicture} // Mock picture
-          alt={'Personal profile'}
-          style={{ marginLeft: '48%', width: '48px', height: '48px' }}
-        />
-        <Typography
-          variant="h6"
-          align="center"
-          style={{
-            fontSize: '1.2em',
-            color: '#d3d3d3',
-          }}
+      <AppBar position="relative" color="transparent">
+        <Tabs
+          value={tab}
+          onChange={handleChangeTab}
+          indicatorColor="primary"
+          centered
+          aria-label="simple tabs example"
         >
-          {personal.name}
-        </Typography>
-        <div
-          style={{
-            display: 'flex',
-            width: '100%',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <PhoneIcon style={{ marginRight: '3px' }} />
-          <Typography
-            align="center"
-            style={{
-              fontSize: '1.2em',
-              color: '#d3d3d3',
-            }}
-          >
-            {Utils.formatPhone(contactPhone)}
-          </Typography>
-        </div>
-        <Typography
-          align="center"
-          style={{ marginBottom: '20px', fontSize: '1.2em', color: '#d3d3d3' }}
-        >
-          CREF: {personal.cref}
-        </Typography>
+          <Tab label="Detalhes da aula" />
+          <Tab label="Alunos" />
+          <Tab label="Item Three" />
+        </Tabs>
+      </AppBar>
 
-        <Divider style={{ marginBottom: '20px' }} />
-
-        {/* Localization and StudentGroup info */}
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'space-around',
-            marginBottom: '20px',
-          }}
-        >
-          <div style={{ display: 'flex', flexFlow: 'column wrap' }}>
-            <Typography gutterBottom variant="h5">
-              <LocationOnIcon /> ENDEREÇO
+      <DialogContent dividers style={{ minHeight: '622px' }}>
+        {/* Show content by Tab */}
+        {tab === 0 && (
+          <>
+            {/* Personal info */}
+            <Typography gutterBottom variant="h5" align="center">
+              <PersonIcon /> PERSONAL
             </Typography>
+            <Avatar
+              src={PersonalPicture} // Mock picture
+              alt={'Personal profile'}
+              style={{ marginLeft: '48%', width: '48px', height: '48px' }}
+            />
             <Typography
-              variant="button"
-              display="block"
-              gutterBottom
+              variant="h6"
+              align="center"
               style={{
                 fontSize: '1.2em',
                 color: '#d3d3d3',
               }}
             >
-              {formatedAddress.street}, {formatedAddress.number} -{' '}
-              {formatedAddress.complemento}
+              {personal.name}
             </Typography>
-            <Typography
-              variant="button"
-              display="block"
-              gutterBottom
+            <div
               style={{
+                display: 'flex',
+                width: '100%',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <PhoneIcon style={{ marginRight: '3px' }} />
+              <Typography
+                align="center"
+                style={{
+                  fontSize: '1.2em',
+                  color: '#d3d3d3',
+                }}
+              >
+                {Utils.formatPhone(contactPhone)}
+              </Typography>
+            </div>
+            <Typography
+              align="center"
+              style={{
+                marginBottom: '20px',
                 fontSize: '1.2em',
                 color: '#d3d3d3',
               }}
             >
-              {formatedAddress.referenceLocation}
-            </Typography>
-            <Typography
-              variant="button"
-              display="block"
-              gutterBottom
-              style={{
-                fontSize: '1.2em',
-                color: '#d3d3d3',
-              }}
-            >
-              {formatedAddress.neighboor}, {formatedAddress.city} -{' '}
-              {formatedAddress.state}
-            </Typography>
-          </div>
-          <div style={{ display: 'flex', flexFlow: 'column wrap' }}>
-            <Typography gutterBottom variant="h5" style={{ color: 'gold' }}>
-              <PriorityHighIcon /> IMPORTANTE
+              CREF: {personal.cref}
             </Typography>
 
-            <Typography
-              variant="button"
-              display="block"
-              gutterBottom
+            <Divider style={{ marginBottom: '20px' }} />
+
+            {/* Localization and StudentGroup info */}
+            <div
               style={{
-                width: '60%',
-                fontSize: '1.2em',
-                color: '#d3d3d3',
+                display: 'flex',
+                justifyContent: 'space-around',
+                marginBottom: '20px',
               }}
             >
-              Vagas restantes:{' '}
-              {students && students.length > 0 ? (
-                <Badge
-                  badgeContent={maxStudentGroupAmount - students.length}
-                  color="primary"
-                >
-                  <GroupIcon />
-                </Badge>
-              ) : (
-                <Badge badgeContent={maxStudentGroupAmount} color="primary">
-                  <GroupIcon />
-                </Badge>
-              )}
-            </Typography>
-
-            <Typography
-              variant="button"
-              display="block"
-              gutterBottom
-              style={{
-                fontSize: '1.2em',
-                color: '#d3d3d3',
-              }}
-            >
-              Quando: {Utils.formatDateTime(eventDateTimeBegin)} ~{' '}
-              {Utils.formatDateTime(eventDateTimeEnd)}
-            </Typography>
-            <Typography
-              variant="button"
-              display="block"
-              gutterBottom
-              style={{
-                fontSize: '1.2em',
-                color: '#d3d3d3',
-              }}
-            >
-              Valor: R${eventPrice}
-            </Typography>
-          </div>
-        </div>
-
-        <Divider style={{ marginBottom: '20px' }} />
-
-        {/* Exercises info */}
-        <Typography gutterBottom variant="h5">
-          <FitnessCenterIcon /> ATIVIDADE
-        </Typography>
-        <div style={{ marginTop: '10px' }}>
-          {exercises.map(({ name, description }, index) => (
-            <ExpansionPanel expanded key={index}>
-              <ExpansionPanelSummary>
-                <Typography variant="h6" gutterBottom>
-                  {name}
+              <div style={{ display: 'flex', flexFlow: 'column wrap' }}>
+                <Typography gutterBottom variant="h5">
+                  <LocationOnIcon /> ENDEREÇO
                 </Typography>
-              </ExpansionPanelSummary>
-              <ExpansionPanelDetails>
                 <Typography
-                  variant="h6"
+                  variant="button"
+                  display="block"
+                  gutterBottom
                   style={{
                     fontSize: '1.2em',
                     color: '#d3d3d3',
                   }}
                 >
-                  {description}
+                  {formatedAddress.street}, {formatedAddress.number} -{' '}
+                  {formatedAddress.complemento}
                 </Typography>
-              </ExpansionPanelDetails>
-            </ExpansionPanel>
-          ))}
-        </div>
+                <Typography
+                  variant="button"
+                  display="block"
+                  gutterBottom
+                  style={{
+                    fontSize: '1.2em',
+                    color: '#d3d3d3',
+                  }}
+                >
+                  {formatedAddress.referenceLocation}
+                </Typography>
+                <Typography
+                  variant="button"
+                  display="block"
+                  gutterBottom
+                  style={{
+                    fontSize: '1.2em',
+                    color: '#d3d3d3',
+                  }}
+                >
+                  {formatedAddress.neighboor}, {formatedAddress.city} -{' '}
+                  {formatedAddress.state}
+                </Typography>
+              </div>
+              <div style={{ display: 'flex', flexFlow: 'column wrap' }}>
+                <Typography gutterBottom variant="h5" style={{ color: 'gold' }}>
+                  <PriorityHighIcon /> IMPORTANTE
+                </Typography>
+
+                <Typography
+                  variant="button"
+                  display="block"
+                  gutterBottom
+                  style={{
+                    width: '60%',
+                    fontSize: '1.2em',
+                    color: '#d3d3d3',
+                  }}
+                >
+                  Vagas restantes:{' '}
+                  {students && students.length > 0 ? (
+                    <Badge
+                      badgeContent={maxStudentGroupAmount - students.length}
+                      color={
+                        maxStudentGroupAmount - students.length !== 0
+                          ? 'primary'
+                          : 'secondary'
+                      }
+                      showZero
+                    >
+                      <GroupIcon />
+                    </Badge>
+                  ) : (
+                    <Badge badgeContent={maxStudentGroupAmount} color="primary">
+                      <GroupIcon />
+                    </Badge>
+                  )}
+                </Typography>
+
+                <Typography
+                  variant="button"
+                  display="block"
+                  gutterBottom
+                  style={{
+                    fontSize: '1.2em',
+                    color: '#d3d3d3',
+                  }}
+                >
+                  Quando: {Utils.formatDateTime(eventDateTimeBegin)} ~{' '}
+                  {Utils.formatDateTime(eventDateTimeEnd)}
+                </Typography>
+                <Typography
+                  variant="button"
+                  display="block"
+                  gutterBottom
+                  style={{
+                    fontSize: '1.2em',
+                    color: '#d3d3d3',
+                  }}
+                >
+                  Valor: R${eventPrice}
+                </Typography>
+              </div>
+            </div>
+
+            <Divider style={{ marginBottom: '20px' }} />
+
+            {/* Exercises info */}
+            <Typography gutterBottom variant="h5">
+              <FitnessCenterIcon /> ATIVIDADE
+            </Typography>
+            <div style={{ marginTop: '10px' }}>
+              {exercises.map(({ name, description }, index) => (
+                <ExpansionPanel expanded key={index}>
+                  <ExpansionPanelSummary>
+                    <Typography variant="h6" gutterBottom>
+                      {name}
+                    </Typography>
+                  </ExpansionPanelSummary>
+                  <ExpansionPanelDetails>
+                    <Typography
+                      variant="h6"
+                      style={{
+                        fontSize: '1.2em',
+                        color: '#d3d3d3',
+                      }}
+                    >
+                      {description}
+                    </Typography>
+                  </ExpansionPanelDetails>
+                </ExpansionPanel>
+              ))}
+            </div>
+          </>
+        )}
+
+        {tab === 1 && (
+          <List>
+            {payments && payments.length > 0 ? (
+              payments.map(({ id, student }) => (
+                <ListItem key={id}>
+                  <ListItemAvatar>
+                    <Avatar>
+                      <ImageIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText primary={student.name} />
+                </ListItem>
+              ))
+            ) : (
+              <Typography>
+                Calma... Daqui a pouco aparece alguém por aqui!
+              </Typography>
+            )}
+          </List>
+        )}
       </DialogContent>
 
       <DialogActions>
         <Button autoFocus onClick={handleClose} color="secondary">
-          {!isEnrolledStudent() ? 'Cancelar' : 'Voltar'}
+          Voltar
         </Button>
         {profile === 'ROLE_STUDENT' && !isEnrolledStudent() && (
           <Button
@@ -309,6 +364,7 @@ DialogStudentGroupDetails.propTypes = {
       name: PropTypes.string.isRequired,
     }).isRequired,
     students: PropTypes.array,
+    payments: PropTypes.array,
   }).isRequired,
 };
 
