5047b84849b8e7ac2fd966ac5dd875af4936caab
fthomasvp <fellipecab@gmail.com>
2020-06-10 05:17:13 -0300

    Add Alert component to display a feedback

--- src/pages/StudentGroup/DialogAddStudentGroup/ThirdStepForm/index.js
+++ src/pages/StudentGroup/DialogAddStudentGroup/ThirdStepForm/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -10,13 +10,8 @@ import IconButton from '@material-ui/core/IconButton';
 import Search from '@material-ui/icons/Search';
 import MenuItem from '@material-ui/core/MenuItem';
 
-import SForm from '../../../../components/Form';
-import { storeThirdStepForm } from '../../../../store/ducks/StudentGroup';
-import {
-  fetchStatesRequest,
-  searchAddressByZipcodeRequest,
-  fetchCitiesByStateIdRequest,
-} from '../../../../store/ducks/Localization';
+import * as StudentGroupReducer from '../../../../store/ducks/StudentGroup';
+import * as LocalizationReducer from '../../../../store/ducks/Localization';
 import YupSchema, {
   street,
   number,
@@ -24,6 +19,8 @@ import YupSchema, {
   city,
   zipcode,
 } from '../../../validators';
+import SForm from '../../../../components/Form';
+import Alert from '../../../../components/Alert';
 
 // Yup Fields Schema
 const ThirdStepSchema = YupSchema({
@@ -35,12 +32,12 @@ const ThirdStepSchema = YupSchema({
 });
 
 const ThirdStepForm = ({ activeStep, handleBack, handleNext }) => {
+  const dispatch = useDispatch();
+
   const { createStudentGroup } = useSelector(state => state.studentGroup);
   const { thirdStepData } = createStudentGroup;
 
   const { states, citiesByState } = useSelector(state => state.localization);
-
-  const dispatch = useDispatch();
 
   const handleClickFindAddressByZipcode = (zipcode, states) => {
     if (zipcode) {
@@ -52,223 +49,291 @@ const ThirdStepForm = ({ activeStep, handleBack, handleNext }) => {
       const fromPage = 'studentgroup';
 
       dispatch(
-        searchAddressByZipcodeRequest(formatedZipCode, states, fromPage)
+        LocalizationReducer.searchAddressByZipcodeRequest(
+          formatedZipCode,
+          states,
+          fromPage
+        )
       );
     } else {
       console.warn('Padrão do CEP Incorreto (e.g. 99999-999)');
     }
   };
 
+  /**
+   * Alert
+   */
+  const errorLocalization = useSelector(state => state.localization.error);
+
+  const [openAlert, setOpenAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState('');
+  const [growTransition, setGrowTransition] = useState(false);
+  const [severity, setSeverity] = useState('error');
+
+  const handleCloseAlert = () => {
+    setAlertMessage('');
+    setOpenAlert(false);
+    setGrowTransition(false);
+
+    dispatch(LocalizationReducer.clearSnackbar());
+  };
+
+  /**
+   * Effects
+   */
   useEffect(() => {
-    (async () => {
-      await dispatch(fetchStatesRequest());
-    })();
-  }, [dispatch]);
+    if (states.length === 0) {
+      dispatch(LocalizationReducer.fetchStatesRequest());
+    }
+  }, [dispatch, states]);
+
+  useEffect(() => {
+    // Error 500 - CEP Not found in API LocationIQ e.g. 54762222
+    if (errorLocalization && errorLocalization.status === 500) {
+      setAlertMessage('Ops! Parece que esse CEP não foi encontrado');
+      setOpenAlert(true);
+      setGrowTransition(true);
+      setSeverity('error');
+    }
+  }, [errorLocalization]);
 
   return (
-    <Formik
-      initialValues={thirdStepData}
-      onSubmit={thirdStepData => {
-        dispatch(storeThirdStepForm(thirdStepData));
-        handleNext();
-      }}
-      validationSchema={ThirdStepSchema}
-      enableReinitialize
-    >
-      {({ values, ...formikProps }) => {
-        const {
-          setFieldValue,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          errors,
-        } = formikProps;
+    <>
+      <Formik
+        initialValues={thirdStepData}
+        onSubmit={thirdStepData => {
+          dispatch(StudentGroupReducer.storeThirdStepForm(thirdStepData));
+          handleNext();
+        }}
+        validationSchema={ThirdStepSchema}
+        enableReinitialize
+      >
+        {({ values, ...formikProps }) => {
+          const {
+            setFieldValue,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            errors,
+          } = formikProps;
 
-        const {
-          zipcode,
-          street,
-          neighborhood,
-          number,
-          complement,
-          referenceLocation,
-          city,
-          state,
-        } = values;
+          const {
+            zipcode,
+            street,
+            neighborhood,
+            number,
+            complement,
+            referenceLocation,
+            city,
+            state,
+          } = values;
 
-        return (
-          <SForm onSubmit={handleSubmit}>
-            <TextField
-              autoFocus
-              id="zipcode"
-              label="CEP"
-              variant="outlined"
-              value={zipcode}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              InputLabelProps={{ style: { color: 'white' } }}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="find address by zipcode"
-                      onClick={() =>
-                        handleClickFindAddressByZipcode(zipcode, states)
-                      }
-                    >
-                      <Search />
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-              style={{ marginBottom: '20px', width: '25%' }}
-              error={errors && errors.zipcode ? true : false}
-              helperText={errors.zipcode || ''}
-            />
-            <TextField
-              id="street"
-              label="Rua"
-              variant="outlined"
-              value={street}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              InputLabelProps={{ style: { color: 'white' } }}
-              style={{ marginBottom: '20px' }}
-              error={errors && errors.street ? true : false}
-              helperText={errors.street || ''}
-            />
-            <TextField
-              id="neighborhood"
-              label="Bairro"
-              variant="outlined"
-              value={neighborhood}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              InputLabelProps={{ style: { color: 'white' } }}
-              style={{ marginBottom: '20px', width: '50%' }}
-              error={errors && errors.neighborhood ? true : false}
-              helperText={errors.neighborhood || ''}
-            />
-            <TextField
-              id="number"
-              label="N°"
-              type="number"
-              variant="outlined"
-              value={number}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              InputLabelProps={{ style: { color: 'white' } }}
-              inputProps={{ min: 1 }}
-              style={{ marginBottom: '20px', width: '10%' }}
-              error={errors && errors.number ? true : false}
-              helperText={errors.number || ''}
-            />
+          return (
+            <SForm onSubmit={handleSubmit}>
+              <TextField
+                autoFocus
+                id="zipcode"
+                label="CEP"
+                variant="outlined"
+                value={zipcode}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                InputLabelProps={{ style: { color: 'white' } }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="find address by zipcode"
+                        onClick={() =>
+                          handleClickFindAddressByZipcode(zipcode, states)
+                        }
+                      >
+                        <Search />
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+                style={{ marginBottom: '20px', width: '25%' }}
+                error={
+                  (errors && errors.zipcode) ||
+                  (errorLocalization && errorLocalization.status === 500)
+                    ? true
+                    : false
+                }
+                helperText={errors.zipcode || ''}
+                FormHelperTextProps={{
+                  style: { width: 'max-content', fontSize: '1.1rem' },
+                }}
+              />
 
-            <TextField
-              id="complement"
-              label="Complemento"
-              variant="outlined"
-              value={complement}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              InputLabelProps={{ style: { color: 'white' } }}
-              style={{ marginBottom: '20px' }}
-            />
-            <TextField
-              id="referenceLocation"
-              label="Ponto de Referência"
-              variant="outlined"
-              value={referenceLocation}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              InputLabelProps={{ style: { color: 'white' } }}
-              style={{ marginBottom: '20px' }}
-            />
+              <TextField
+                id="street"
+                label="Rua"
+                variant="outlined"
+                value={street}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                InputLabelProps={{ style: { color: 'white' } }}
+                style={{ marginBottom: '20px' }}
+                error={errors && errors.street ? true : false}
+                helperText={errors.street || ''}
+                FormHelperTextProps={{
+                  style: { width: 'max-content', fontSize: '1.1rem' },
+                }}
+              />
 
-            {/* Serão Select Fields */}
-            <TextField
-              id="state"
-              select
-              label="Estado"
-              variant="outlined"
-              value={state}
-              onChange={async evt => {
-                const selectedState = evt.target.value;
+              <TextField
+                id="neighborhood"
+                label="Bairro"
+                variant="outlined"
+                value={neighborhood}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                InputLabelProps={{ style: { color: 'white' } }}
+                style={{ marginBottom: '20px', width: '50%' }}
+                error={errors && errors.neighborhood ? true : false}
+                helperText={errors.neighborhood || ''}
+                FormHelperTextProps={{
+                  style: { width: 'max-content', fontSize: '1.1rem' },
+                }}
+              />
 
-                await setFieldValue('state', selectedState);
+              <TextField
+                id="number"
+                label="N°"
+                type="number"
+                variant="outlined"
+                value={number}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                InputLabelProps={{ style: { color: 'white' } }}
+                inputProps={{ min: 1 }}
+                style={{ marginBottom: '20px', width: '10%' }}
+              />
 
-                return await dispatch(
-                  fetchCitiesByStateIdRequest(selectedState)
-                );
-              }}
-              InputLabelProps={{ style: { color: 'white' } }}
-              SelectProps={{
-                autoWidth: true,
-              }}
-              style={{ marginBottom: '20px', width: '10%' }}
-              error={errors && errors.state ? true : false}
-              helperText={errors.state || ''}
-            >
-              {states &&
-                states.map(state => (
-                  <MenuItem key={state.id} value={state}>
-                    {state.initials}
-                  </MenuItem>
-                ))}
-            </TextField>
+              <TextField
+                id="complement"
+                label="Complemento"
+                variant="outlined"
+                value={complement}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                InputLabelProps={{ style: { color: 'white' } }}
+                style={{ marginBottom: '20px' }}
+              />
+              <TextField
+                id="referenceLocation"
+                label="Ponto de Referência"
+                variant="outlined"
+                value={referenceLocation}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                InputLabelProps={{ style: { color: 'white' } }}
+                style={{ marginBottom: '20px' }}
+              />
 
-            <TextField
-              id="city"
-              select
-              label="Cidade"
-              variant="outlined"
-              value={city}
-              onChange={evt => setFieldValue('city', evt.target.value)}
-              InputLabelProps={{ style: { color: 'white' } }}
-              SelectProps={{
-                autoWidth: true,
-              }}
-              style={{ marginBottom: '20px', width: '35%' }}
-              error={errors && errors.city ? true : false}
-              helperText={errors.city || ''}
-            >
-              {citiesByState && citiesByState.length > 0 ? (
-                citiesByState.map(city => (
-                  <MenuItem key={city.id} value={city.name}>
-                    {city.name}
-                  </MenuItem>
-                ))
-              ) : (
-                <MenuItem value={city}>{city}</MenuItem>
-              )}
-            </TextField>
+              {/* Serão Select Fields */}
+              <TextField
+                id="state"
+                select
+                label="Estado"
+                variant="outlined"
+                value={state}
+                onChange={async evt => {
+                  const selectedState = evt.target.value;
 
-            {/* Step Control Buttons */}
-            <div
-              style={{
-                width: '100%',
-                display: 'flex',
-                justifyContent: 'space-around',
-              }}
-            >
-              <div>
-                <Button
-                  disabled={activeStep === 0}
-                  color="secondary"
-                  onClick={handleBack}
-                >
-                  Voltar
-                </Button>
+                  await setFieldValue('state', selectedState);
+
+                  return await dispatch(
+                    LocalizationReducer.fetchCitiesByStateIdRequest(
+                      selectedState
+                    )
+                  );
+                }}
+                InputLabelProps={{ style: { color: 'white' } }}
+                SelectProps={{
+                  autoWidth: true,
+                }}
+                style={{ marginBottom: '20px', width: '10%' }}
+                error={errors && errors.state ? true : false}
+                helperText={errors.state || ''}
+                FormHelperTextProps={{
+                  style: { width: 'max-content', fontSize: '1.1rem' },
+                }}
+              >
+                {states &&
+                  states.map(state => (
+                    <MenuItem key={state.id} value={state}>
+                      {state.initials}
+                    </MenuItem>
+                  ))}
+              </TextField>
+
+              <TextField
+                id="city"
+                select
+                label="Cidade"
+                variant="outlined"
+                value={city}
+                onChange={evt => setFieldValue('city', evt.target.value)}
+                InputLabelProps={{ style: { color: 'white' } }}
+                SelectProps={{
+                  autoWidth: true,
+                }}
+                style={{ marginBottom: '20px', width: '35%' }}
+                error={errors && errors.city ? true : false}
+                helperText={errors.city || ''}
+                FormHelperTextProps={{
+                  style: { width: 'max-content', fontSize: '1.1rem' },
+                }}
+              >
+                {citiesByState && citiesByState.length > 0 ? (
+                  citiesByState.map(city => (
+                    <MenuItem key={city.id} value={city.name}>
+                      {city.name}
+                    </MenuItem>
+                  ))
+                ) : (
+                  <MenuItem value={city}>{city}</MenuItem>
+                )}
+              </TextField>
+
+              {/* Step Control Buttons */}
+              <div
+                style={{
+                  width: '100%',
+                  display: 'flex',
+                  justifyContent: 'space-around',
+                }}
+              >
+                <div>
+                  <Button
+                    disabled={activeStep === 0}
+                    color="secondary"
+                    onClick={handleBack}
+                  >
+                    Voltar
+                  </Button>
+                </div>
+                <div>
+                  <Button variant="contained" color="primary" type="submit">
+                    Concluir
+                  </Button>
+                </div>
               </div>
-              <div>
-                <Button variant="contained" color="primary" type="submit">
-                  Concluir
-                </Button>
-              </div>
-            </div>
-          </SForm>
-        );
-      }}
-    </Formik>
+            </SForm>
+          );
+        }}
+      </Formik>
+
+      <Alert
+        open={openAlert}
+        handleClose={handleCloseAlert}
+        growTransition={growTransition}
+        message={alertMessage}
+        severity={severity}
+      />
+    </>
   );
 };
 
