a753f4f9f3498b57be440bf2671dc95923ea931a
fthomasvp <fellipecab@gmail.com>
2020-06-21 10:15:16 -0300

    Delete unused files

--- src/components/Form/ErrorMessageInput/styles.js
+++
@@ -1,8 +0,0 @@
-import Styled from 'styled-components';
-
-export const ErrorMessageInput = Styled.span`
-  color: ${({ color }) => color || 'yellow'};
-  font-weight: ${({ fontWeight }) => fontWeight || 'bold'};
-  margin-top: ${({ marginTop }) => marginTop || '5px'};
-  font-size: 13px;
-`;
