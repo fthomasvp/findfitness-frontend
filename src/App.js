a3830b0ceed8d0174f973d09e4cbe1558a5f9411
fthomasvp <fellipecab@gmail.com>
2020-06-22 01:31:56 -0300

    Remove CssBaseline and ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
+
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import Routes from './routes';
 import { store, persistor } from './store';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { createMuiTheme } from '@material-ui/core';
-import { ThemeProvider } from '@material-ui/styles';
 
-const theme = createMuiTheme({
-  palette: {
-    type: 'dark',
-  },
-});
+import Routes from './routes';
 
 const App = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Routes />
-        </ThemeProvider>
+        <Routes />
       </PersistGate>
     </Provider>
   );
