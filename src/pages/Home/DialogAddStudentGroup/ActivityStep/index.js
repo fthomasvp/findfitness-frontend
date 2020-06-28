1361529f114b8de3b5e3367d075c9f921cba9236
fthomasvp <fellipecab@gmail.com>
2020-06-28 05:36:35 -0300

    Improve import from material table icons [DRY]

--- src/pages/Home/DialogAddStudentGroup/ActivityStep/index.js
+++ src/pages/Home/DialogAddStudentGroup/ActivityStep/index.js
@@ -1,48 +1,16 @@
 /* eslint-disable react/display-name */
-import React, { forwardRef, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import MaterialTable from 'material-table';
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
 import Button from '@material-ui/core/Button';
 import Radio from '@material-ui/core/Radio';
 
 import { storeSecondStepForm } from '../../../../store/ducks/StudentGroup';
 import { searchExercisesRequest } from '../../../../store/ducks/Exercise';
 import { ContainerActionButtons, ActionButtons } from '../styles';
-
-const tableIcons = {
-  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  DetailPanel: forwardRef((props, ref) => (
-    <ChevronRight {...props} ref={ref} />
-  )),
-  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  PreviousPage: forwardRef((props, ref) => (
-    <ChevronLeft {...props} ref={ref} />
-  )),
-  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
-};
+import { tableIcons } from '../../../../assets/js/material-table-icons';
 
 const ActivityStep = ({ activeStep, handleBack, handleNext }) => {
   const dispatch = useDispatch();
@@ -107,7 +75,7 @@ const ActivityStep = ({ activeStep, handleBack, handleNext }) => {
       >
         <MaterialTable
           icons={tableIcons}
-          title="Selecione uma atividade"
+          title=""
           columns={columns}
           data={exercises}
           detailPanel={rowData => {
@@ -154,6 +122,7 @@ const ActivityStep = ({ activeStep, handleBack, handleNext }) => {
               fontSize: '1.2rem',
             },
             paginationType: 'stepped',
+            pageSize: 4,
           }}
           localization={{
             toolbar: {
@@ -162,7 +131,7 @@ const ActivityStep = ({ activeStep, handleBack, handleNext }) => {
             },
             body: {
               emptyDataSourceMessage:
-                'Hmmm... Parece que você ainda não entrou numa aula ;-(',
+                'Hmmm... Parece que você ainda não entrou numa aula :(',
             },
             pagination: {
               firstTooltip: 'Primeira página',
--- src/pages/StudentGroup/index.js
+++ src/pages/StudentGroup/index.js
@@ -1,47 +1,15 @@
 /* eslint-disable react/display-name */
-import React, { forwardRef, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import MaterialTable from 'material-table';
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
 import Typography from '@material-ui/core/Typography';
 import OpenInBrowserIcon from '@material-ui/icons/OpenInBrowser';
 
 import { fetchPaymentsRequest } from '../../store/ducks/Student';
 import Utils from '../../utils';
 import DialogPaymentDetails from '../../components/DialogPaymentDetails';
-
-const tableIcons = {
-  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  DetailPanel: forwardRef((props, ref) => (
-    <ChevronRight {...props} ref={ref} />
-  )),
-  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  PreviousPage: forwardRef((props, ref) => (
-    <ChevronLeft {...props} ref={ref} />
-  )),
-  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
-};
+import { tableIcons } from '../../assets/js/material-table-icons';
 
 const StudentGroup = () => {
   const dispatch = useDispatch();
@@ -62,8 +30,10 @@ const StudentGroup = () => {
       title: 'Quando',
       field: 'studentGroup[eventDateTimeBegin]',
       defaultSort: 'desc',
+      // eslint-disable-next-line react/prop-types
       render: ({ studentGroup }) => (
         <Typography style={{ fontSize: '1.2rem' }}>
+          {/* eslint-disable-next-line react/prop-types */}
           {Utils.formatDateTime(studentGroup.eventDateTimeBegin)}
         </Typography>
       ),
@@ -79,8 +49,10 @@ const StudentGroup = () => {
     {
       title: 'Preço',
       field: 'studentGroup[eventPrice]',
+      // eslint-disable-next-line react/prop-types
       render: ({ studentGroup }) => (
         <Typography style={{ fontSize: '1.2rem' }}>
+          {/* eslint-disable-next-line react/prop-types */}
           R$ {studentGroup.eventPrice}
         </Typography>
       ),
@@ -134,7 +106,7 @@ const StudentGroup = () => {
           },
           body: {
             emptyDataSourceMessage:
-              'Hmmm... Parece que você ainda não entrou numa aula ;-(',
+              'Hmmm... Parece que você ainda não entrou numa aula :(',
           },
           pagination: {
             firstTooltip: 'Primeira página',
