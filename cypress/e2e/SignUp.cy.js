131dfa3c26fe55211ed216a02b00ca99128fdc00
fthomasvp <fellipecab@gmail.com>
2022-08-16 03:14:29 -0300

    WIP: improve Sign Up test page
    - Add command to set a date in a Date Picker component.

--- cypress/e2e/SignUp.cy.js
+++ cypress/e2e/SignUp.cy.js
@@ -2,22 +2,66 @@
 
 describe('Sign Up', () => {
   beforeEach(() => {
-    cy.visit('/');
+    cy.visit('/signup');
   });
 
   it('should be able to sign up a student successfully', () => {
-    const { email, password } = {
+    const { email, password, birthdate, cpf, name, phone, gender } = {
       email: 'fellipecab@gmail.com',
       password: '1234f@',
+      birthdate: '1993-09-20',
+      cpf: '09735460009',
+      name: 'Fellipe Thomás',
+      phone: '81987403692',
+      gender: 'MASCULINO',
     };
 
-    cy.get('a[href="/signup"]').click();
+    //#region Select "ESTUDANTE" profile type
+    cy.get('.MuiTabs-flexContainer').within(() => {
+      cy.get('button')
+        .first()
+        .and('have.text', 'ESTUDANTE')
+        .click();
+    });
+    //#endregion
 
-    cy.location('pathname').should('include', 'signup');
+    cy.get('#sPanelActions').within(() => {
+      cy.get('button')
+        .last()
+        .click();
+    });
 
-    // Selecionar "Estudante"
-    // Preencher informações pessoais corretamente
-    // Preencher informações do endereço corretamente
-    // Finalizar cadastro
+    cy.location('pathname').should('include', 'signup/userform');
+
+    // Fulfill personal information correctly
+    cy.get('input[id="name"]').type(name);
+    cy.get('input[id="phone"]').type(phone);
+    cy.get('input[id="cpf"]').type(cpf);
+
+    cy.get('.MuiTabs-flexContainer').within(() => {
+      cy.get('button')
+        .contains(gender)
+        .click();
+    });
+
+    // Create an command to update birthdate input
+    cy.setDatePicker('birthdate', birthdate);
+
+    cy.get('.MuiDialogActions-root').within(() => {
+      cy.get('button')
+        .last()
+        .click();
+    });
+
+    cy.get('input[id="email"]').type(email);
+    cy.get('input[id="password"]').type(password);
+
+    cy.get('button[type=submit]').click();
+
+    // Fulfill address information correctly
+    cy.location('pathname').should('include', 'signup/addressform');
+
+    // Finish sign up process
+    cy.get('button[type=submit]').click();
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -9,6 +9,21 @@
 // ***********************************************
 //
 //
+const MONTHS = [
+  'janeiro',
+  'fevereiro',
+  'março',
+  'abril',
+  'maio',
+  'junho',
+  'julho',
+  'agosto',
+  'setembro',
+  'outubro',
+  'novembro',
+  'dezembro',
+];
+
 // -- This is a parent command --
 Cypress.Commands.add('login', (email, password) => {
   cy.get('#email').type(email);
@@ -16,6 +31,65 @@ Cypress.Commands.add('login', (email, password) => {
 
   cy.get('button[type=submit]').click();
 });
+
+Cypress.Commands.add('setDatePicker', (id, date) => {
+  const [year, month, day] = date.split('-');
+
+  cy.get(`input[id=${id}]`).click();
+
+  //#region Select the year
+  cy.get('.MuiPickersDatePickerRoot-toolbar').within(() => {
+    cy.get('button')
+      .first()
+      .click();
+  });
+
+  cy.get('.MuiPickersYearSelection-container')
+    .contains(year)
+    .click();
+  //#endregion
+
+  //#region Select the month
+  cy.get('.MuiPickersCalendarHeader-switchHeader').within(() => {
+    let currentMonthNumber = 0;
+
+    cy.get('div')
+      .invoke('text')
+      .then(text => {
+        const [monthText] = text.split(' ');
+
+        currentMonthNumber = MONTHS.findIndex(item => item === monthText) + 1;
+
+        if (Number(month) !== currentMonthNumber) {
+          const monthDiff = Number(month) - currentMonthNumber;
+
+          if (monthDiff > 0) {
+            for (let index = 0; index < monthDiff; index++) {
+              cy.get('button')
+                .last()
+                .click();
+            }
+          }
+
+          if (monthDiff < 0) {
+            for (let index = 0; index < Math.abs(monthDiff); index++) {
+              cy.get('button')
+                .first()
+                .click();
+            }
+          }
+        }
+      });
+  });
+  //#endregion
+
+  //#region Select the day
+  cy.get('.MuiPickersCalendar-week')
+    .contains(Number(day))
+    .click();
+  //#endregion
+});
+
 //
 //
 // -- This is a child command --
@@ -28,4 +102,3 @@ Cypress.Commands.add('login', (email, password) => {
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
